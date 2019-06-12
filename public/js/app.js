fbc173ca3cf49bc278ed9f8f716dc0cd5a5473ac
trirahmataribowo <Tri.aribowo@btpn.com>
2019-06-12 22:23:39 +0700

    update app.js in public

--- public/js/app.js
+++ public/js/app.js
@@ -24,12 +24,13 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-messageOne.textContent = 'loading ..';
-messageTwo.textContent = '';
 
-weatherForm.addEventListener('submit', (res) =>{
-    res.preventDefault();
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
     const location = search.value;
+    
+    messageOne.textContent = 'loading ..';
+    messageTwo.textContent = '';
 
     fetch('/weather?address='+location).then((response) =>{
         response.json().then((data) =>{
